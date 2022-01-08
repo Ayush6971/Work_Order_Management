7ce7f7afbc7031617744b4634f3ca3527aed2e6a
Ayush6971 <ayushsahu76@gmail.com>
2022-01-09 02:11:04 +0530

    dashboard inital commit

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -8,11 +8,7 @@ const dashboard = async (req, res) => {
     const findCurrentUser = await userFindOne({ id: currentUser.id });
     if (!findCurrentUser) return res.status(400).send("User not found!");
 
-    return res
-      .status(200)
-      .send(
-        '<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>'
-      );
+    return res.render('dashboard')
   } catch (error) {
     console.log(
       "🚀 ~ file: AuthController.js ~ line 29 ~ login ~ error",
